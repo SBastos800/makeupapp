bee001805a203e85e1c0b220f84dff173b66fe1b
SBastos800 <soniavsbastos@gmail.com>
2020-05-18 16:26:10 +0100

    created app with containers and components folders to start

---
+++ src/containers/App/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from "./App.module.scss";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
+}
+
+
