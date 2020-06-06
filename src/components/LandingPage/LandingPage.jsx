73cb1ce5ec5f6b400aa53b0d41caab248494db88
SBastos800 <soniavsbastos@gmail.com>
2020-06-06 20:17:46 +0100

    placed images in the home page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -5,17 +5,17 @@ import styles from "./LandingPage.module.scss";
 export default class LandingPage extends Component {
     render() {
         return (
-            <section>
+            <section className={styles.homePage}>
                 <h1>Welcome to Our Website</h1>
-                <div>
-                    <div>
-                       <div className={styles.one}></div>
-                       <div className={styles.two}></div>
-                       <div className={styles.three}></div>
-                       <div className={styles.four}></div>
+                <div className={styles.gridWrapper}>
+                    <div className={styles.pictures}>
+                        <div className={styles.one}></div>
+                        <div className={styles.two}></div>
+                        <div className={styles.three}></div>
+                        <div className={styles.four}></div>
                     </div>
                 </div>
-            </section> 
+            </section>
 
         );
     }
