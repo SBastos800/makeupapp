d265438435a24ac0e40161bdfc8ead325676f8b3
SBastos800 <soniavsbastos@gmail.com>
2020-06-06 18:03:49 +0100

    signOut stylished and begining to stylish searchBar

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -17,47 +17,48 @@ export default class Products extends Component {
 
     setSearchText = (event) => {
         const searchText = event.target.value;
-        this.setState({searchText}, this.filterProducts)
+        this.setState({ searchText }, this.filterProducts)
     }
 
     filterProducts = () => {
         let filteredProducts = this.state.myProducts.filter(product => {
             return product.name
-            .toUpperCase()
-            .includes(this.state.searchText.toUppercase());
+                .toUpperCase()
+                .includes(this.state.searchText.toUppercase());
         })
         this.setState({ filteredProducts });
     }
 
     componentDidMount() {
         fetch(this.state.url)
-        .catch(err => {
-            console.log(err);
-        })
-        .then(res => res.json())
-        .then((data) => {
-            this.setState({ 
-                myProducts: data,
-                filteredProducts: data
-             })
-             console.log(data);
-        });
-        
+            .catch(err => {
+                console.log(err);
+            })
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({
+                    myProducts: data,
+                    filteredProducts: data
+                })
+                console.log(data);
+            });
+
     }
 
     render() {
         console.log(this.state.myProducts)
-        return(
+        return (
             <section className={styles.productsWrapper}>
                 <h1>Maybelline Products</h1>
                 <section className={styles.signOut} onClick={this.props.signOut}>
                     <button>Sign Out</button>
                 </section>
                 <SearchBar searchText={this.state.searchText} setSearchText={this.setSearchText} />
-                
-                {this.state.myProducts.map((person, index) => (
-                    <Product productData={person} key={index} />
-                ))}
+                <section className={styles.individualProduct}>
+                    {this.state.myProducts.map((person, index) => (
+                        <Product productData={person} key={index} />
+                    ))}
+                </section>
             </section>
         );
     }
