8390828a2b30b57dcf446506ef1fd97ab036d7c6
SBastos800 <soniavsbastos@gmail.com>
2020-06-12 17:43:47 +0100

    stylished login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,11 +3,13 @@ import styles from "./Login.module.scss";
 
 export default class Login extends Component {
     render() {
-        return(
+        return (
             <section onClick={this.props.signIn} className={styles.google}>
-            <h1>Hi!! We are excited that you have chosen us and you're ready to take a look in our fantastic products!</h1>
-            <h2>Please click in the button below to sign in and then go to Products</h2>
-                <button>Sign in with google</button>
+                <section className={styles.loginInstructionsWrapper}>
+                    <h1>Hi!! We are excited that you have chosen us and you're ready to take a look in our fantastic products!</h1>
+                    <h2>Please click the button below to sign in and then go to Products</h2>
+                    <button>Sign in with google</button>
+                </section>
             </section>
         )
     }
